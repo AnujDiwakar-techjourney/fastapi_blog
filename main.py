16892ce866195f573aedf53628613ed46465b13e
anuj diwakar <anujdiwakar745@gmail.com>
2026-08-01 11:45:44 +0530

    How to use tempelate in fastapi

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI,requests
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
-
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates=Jinja2Templates(directory="templates") 
 
 posts: list[dict] = [
     {
@@ -22,11 +23,11 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/",response_class=HTMLResponse,include_in_schema=False)
-@app.get("/posts",response_class=HTMLResponse,include_in_schema=False)
+@app.get("/",include_in_schema=False,name="home")
+@app.get("/posts",include_in_schema=False,name="posts")
 
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+def home(request: requests.Request):
+    return templates.TemplateResponse(request,"home.html",{"posts": posts,"title": "Home"},)
 
 @app.get("/api/posts")
 def get_posts():
